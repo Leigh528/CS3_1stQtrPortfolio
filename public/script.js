f5f6048b41ee432b769ea781882727cdc693ff82
Leigh528 <b2029lmamenor@pshs.edu.ph>
2025-08-05 08:49:30 +0800

    Q11stNGBeMenor.html.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
-// no script needed
+// no script tag here
+
+let age = 15;
+let maxAge = 100;
+let numPerDay = 1;
+let totalRequired = (numPerDay * 365) *(maxAge - age);
+document.write("You will need 1 bag of chips to last you until the ripe old age of 100")
