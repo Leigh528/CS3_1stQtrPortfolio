6c09bff4d3915de521c7b1ce11de61457c8b4938
Leigh528 <b2029lmamenor@pshs.edu.ph>
2025-08-12 08:35:35 +0800

    st1

--- public/script.js
+++ public/script.js
@@ -1,7 +1,17 @@
 // no script tag here
 
-let age = 15;
-let maxAge = 100;
-let numPerDay = 1;
+let age;
+let maxAge;
+let numPerDay;
 let totalRequired = (numPerDay * 365) *(maxAge - age);
-document.write("You will need 1 bag of chips to last you until the ripe old age of 100")
+
+
+document.write("You will need bag of chips to last you until the ripe old age of 100");
+
+// Another activity here
+
+let Age=window.prompt("Enter your age","Enter here");
+ window.confirm('Is' $(age) 'true?');
+
+
+
